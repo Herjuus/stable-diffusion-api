794f11187415db956b255b0ecae08419075a7b6e
Herjus <herjus420@gmail.com>
2023-09-13 15:39:27 +0200

    first commit

---
+++ api.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+from io import BytesIO
+import base64
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+device = "cpu"
+model_id = "runwayml/stable-diffusion-v1-5"
+pipe = StableDiffusionPipeline.from_pretrained(model_id)
+pipe.to(device)
+
+@app.get("/")
+def generate(prompt: str):
+    with autocast(device):
+        image = pipe(prompt, num_inference_steps=1, guidance_scale=6).images[0]
+
+    image.save("image.png")
+    buffer = BytesIO()
+    image.save(buffer, format="PNG")
+    imgstr = base64.b64encode(buffer.getvalue())
+
+    return Response(content=imgstr, media_type="image/png")
