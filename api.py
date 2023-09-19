31c501ea5240a714c637e5c4999d53b54a85a88f
Herjus <herjus420@gmail.com>
2023-09-19 15:02:30 +0200

    A

--- api.py
+++ api.py
@@ -87,3 +87,14 @@ async def generate(prompt: str):
     queue.remove(id)
 
     return ReturnObject(id=id, prompt=prompt, image=imgstr)
+
+@app.sio.on("prompt")
+async def listenprompt(sid, *args, **kwargs):
+    print(args)
+
+async def emit(prompt): 
+    currentTime = datetime.datetime.now()
+    time = f"{currentTime.hour}:{currentTime.minute}"
+
+    await app.sio.emit("prompt", { prompt, time })
+
