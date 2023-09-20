7765a20cf6b7e6b45a3e43d44995c6aae344f1fa
Herjus <herjus420@gmail.com>
2023-09-20 09:40:00 +0200

    Update api.py

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*'],
 )
-socket = SocketManager(app=app, cors_allowed_origins=["*"], async_mode="asgi")
+socket = SocketManager(app=app, cors_allowed_origins=["*", "localhost:3000", "10.58.177.194"], async_mode="asgi")
 
 #----------STABLE-DIFFUSION INIT----------#
 device = "cuda"
